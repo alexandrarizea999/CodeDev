57b3cc97da7a50ab26aea55c58c81a4870da3b02
alexandrarizea999 <alexandrarizea222@yahoo.com>
2025-03-27 12:20:55 +0200

    form

---
+++ vue-tutorial-adv/modal-project/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+// we directly import this from the vue librarys
+createApp(App).mount("#app");
---
+++ web-form/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<h1>Signup Form</h1>
+	<SignupForm />
+</template>
+
+<script>
+
+import SignupForm from './components/SignupForm.vue';
+
+export default {
+	name: 'App',
+	components: {
+		SignupForm
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
